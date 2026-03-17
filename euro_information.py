46988e423755a400580c5d5f88028bf554ca70a6
Dave Nesbitt <git@github.com>
2026-03-17 19:01:59 +0000

    split out telegram commands

---
+++ euro_information.py
@@ -0,0 +1,91 @@
+
+COUNTRY_FLAGS = {
+    "norway": "🇳🇴", "malta": "🇲🇹", "serbia": "🇷🇸", "latvia": "🇱🇻", "portugal": "🇵🇹", "ireland": "🇮🇪", "croatia": "🇭🇷",
+    "switzerland": "🇨🇭", "israel": "🇮🇱", "moldova": "🇲🇩", "sweden": "🇸🇪", "azerbaijan": "🇦🇿", "czechia": "🇨🇿",
+    "netherlands": "🇳🇱", "finland": "🇫🇮", "denmark": "🇩🇰", "armenia": "🇦🇲", "romania": "🇷🇴", "estonia": "🇪🇪",
+    "belgium": "🇧🇪", "cyprus": "🇨🇾", "iceland": "🇮🇸", "greece": "🇬🇷", "poland": "🇵🇱", "slovenia": "🇸🇮", "georgia": "🇬🇪", 
+    "san marino": "🇸🇲", "austria": "🇦🇹", "albania": "🇦🇱", "lithuania": "🇱🇹", "australia": "🇦🇺", "france": "🇫🇷", 
+    "germany": "🇩🇪", "italy": "🇮🇹", "spain": "🇪🇸", "ukraine": "🇺🇦", "united kingdom": "🇬🇧", "luxembourg": "🇱🇺",
+     "montenegro": "🇲🇪"
+}
+
+SEMI_FINAL_ONE = [
+    "Croatia",
+    "Finland",
+    "Georgia",
+    "Greece",
+    "Moldova",
+    "Portugal",
+    "Sweden",
+    "Belgium",
+    "Estonia",
+    "Israel",
+    "Lithuania",
+    "Montenegro",
+    "Poland",
+    "San Marino",
+    "Serbia"
+]
+
+SEMI_FINAL_TWO = [
+    "Armenia",
+    "Azerbaijan",
+    "Bulgaria",
+    "Czechia",
+    "Luxembourg",
+    "Romania",
+    "Switzerland",
+    "Albania",
+    "Australia",
+    "Cyprus",
+    "Denmark",
+    "Latvia",
+    "Malta",
+    "Norway",
+    "Ukraine"
+]
+SEMI_FINAL_ONE_ELIMINATED = []
+SEMI_FINAL_TWO_ELIMINATED = []
+RESULTS = {}
+
+# eurovision 2025
+SONGS = {
+    "albania": "Nân",
+    "armenia": "Paloma rumba",
+    "australia": "Eclipse",
+    "austria": "Tanzschein",
+    "azerbaijan": "Just Go",
+    "belgium": "Dancing on the Ice",
+    "bulgaria": "Bangaranga",
+    "croatia": "Andromeda",
+    "cyprus": "Jalia",
+    "denmark": "Før vi går hjem",
+    "estonia": "Too Epic to Be True",
+    "finland": "Liekinheitin",
+    "france": "Regarde !",
+    "georgia": "On Replay",
+    "germany": "Fire",
+    "greece": "Φέρτο",
+    "israel": "Michelle",
+    "italy": "Per sempre si",
+    "latvia": "Ēnā",
+    "lithuania": "Sólo quiero más",
+    "luxembourg": "Mother Nature",
+    "malta": "Bella",
+    "moldova": "Viva, Moldova!",
+    "montenegro": "Нова зора",
+    "norway": "Ya Ya Ya",
+    "poland": "Pray",
+    "portugal": "Rosa",
+    "romania": "Choke Me",
+    "san marino": "Superstar",
+    "serbia": "Крај мене",
+    "sweden": "My System",
+    "switzerland": "Alice",
+    "ukraine": "Рідним",
+    "united kingdom": "Eins, zwei, drei"
+}
+SONG_URLS = {
+}
+COUNTRIES = list(SONGS.keys())
+
